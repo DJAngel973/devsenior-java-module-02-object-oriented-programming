6b98f81e89b9f736dda5d97a93b578ade6f55ac1
DJAngel973 <angel.djuancho@gmail.com>
2025-07-14 23:57:25 -0500

    feat(PrimaryMenuUI): create class for show primary menu

---
+++ src/com/modulo2/classoneandtwo/view/PrimaryMenuUI.java
@@ -0,0 +1,17 @@
+package com.modulo2.classoneandtwo.view;
+
+public class PrimaryMenuUI {
+
+    // Method for primary menu
+    public void showMenu(){
+        System.out.print("""
+                🤘🇨🇴🤘🇨🇴🤘    Bienvenidos al modulo 2 DevSenior    🤘🇨🇴🤘🇨🇴🤘
+                Podrás escoger alguna opción para interactuar
+                con ejercicios y los pequeños proyectos realizados.
+                1. Clase 1: Clases, Objetos, Constructores y Encapsulamiento
+                2. Terminar
+                
+                Escoge una opción:
+                """);
+    }
+}
