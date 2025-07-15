febc53ea0f12a21f786fc48d386227543e5c921c
DJAngel973 <angel.djuancho@gmail.com>
2025-07-14 23:59:05 -0500

    feat(menuClassUI): create class for submenu

---
+++ src/com/modulo2/classoneandtwo/view/MenuClass1UI.java
@@ -0,0 +1,18 @@
+package com.modulo2.classoneandtwo.view;
+
+public class MenuClass1UI {
+
+    // Method for menu first class
+    public void firstClass(){
+        System.out.println("""
+                En esta primer clase vimos como crear una clase simple, tomamos como ejemplo 'Book'
+                la cual definimos los atributos, el constructor para el encapsulamiento
+                mas los métodos necesarios, con ello desde el main o una clase que genere la lógica
+                cree el objeto y se puede interactuar con el.
+                1. Ejemplo Book
+                2. Primer ejercicio
+                3. Volver menu principal
+                Escoge una opción valida
+                """);
+    }
+}
